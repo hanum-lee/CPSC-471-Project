cfc872e6bc9802b79be74c2c62eacba3e19b6c2d
sunahkim15 <sunah.kim@ucalgary.ca>
2018-11-20 16:49:09 -0700

    started on the javascript: able to get search input into variable and hide/show manage and login button

--- Frontend/js/JavaScript.js
+++ Frontend/js/JavaScript.js
@@ -1,1 +1,63 @@
 //document.write("Hello World");
+
+// use variable to hide certain elements
+let userLogin = false;
+
+let loginButton = document.getElementById("logButton");
+let manageButton = document.getElementById("manageButton");
+if (userLogin) {
+  loginButton.innerHTML = "Logout";
+  manageButton.className = "";
+}
+else {
+  loginButton.innerHTML = "Login/Register";
+  manageButton.className = "hide";
+}
+
+
+/***********************
+Searchbar
+***********************/
+let searchBar =  document.getElementById('mainSearch');
+let searchButton = document.getElementById('searchButton');
+let searchMssg = document.getElementById('searchMessage');
+let searchInput = '';
+
+searchButton.addEventListener("click",search);
+
+function search() {
+  let searchInput = searchBar.value;
+  if (searchInput == '') {
+    searchMssg.innerHTML = "aw nothing to search :(";
+  }
+  else {
+    searchMssg.innerHTML = "Foooooood";
+    console.log(searchInput);
+  }
+}
+
+/***********************
+Login/Register
+***********************/
+
+
+
+// connect to server
+/*
+console.log("open: ");
+var ws = new WebSocket("ws://127.0.0.1:8081");
+ws.onopen = function (event) {
+console.log('Connection is open ...');
+ws.send("Hello Server");
+};
+ws.onerror = function (err) {
+console.log('err: ', err);
+}
+ws.onmessage = function (event) {
+console.log(event.data);
+document.body.innerHTML += event.data + '&lt;br&gt;';
+};
+ws.onclose = function() {
+console.log("Connection is closed...");
+}
+*/
