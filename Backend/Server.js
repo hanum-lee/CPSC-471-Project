f9a82ceeef0b05a112e91e0408420c6fe37a1540
Sally <sally_blue_star@yahoo.ca>
2018-11-28 13:50:57 -0700

    Made it so clicking on logo on main page links to itself/refresh page

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,3 +4,17 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
 }).listen(8080);
+
+/*
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: "localhost", 
+	user: "",		//need to add in id
+	password: ""	// need to add in a password
+});
+con.connect(function(err) {
+	if (err)throw err;
+	console.log("Connected!");
+});
+*/
