e4e3ca542abfe96098eed15271b29c7e3574f76e
sunahkim15 <sunah.kim@ucalgary.ca>
2018-11-30 13:44:16 -0700

    finished dealing with send/receive info from server - need to test later with real data

--- Frontend/js/loginpage.js
+++ Frontend/js/loginpage.js
@@ -1,28 +1,22 @@
-var req = new XMLHttpRequest();
-
 function userLog() {
-  let username = $("#logUsername").val();
-  let password = $("#logPassword").val();
   var logSuccess;
-
-  var logInfo = {
-    user: username,
-    pass: password
+  /* logSuccess form
+  logSuccess = {
+    userexists: <true or false value>
   }
+  */
 
-  var logInfoString = JSON.stringify(logInfo);
-
-
-  // sending information to server using xml
-
+  // sending information to server
   var logInfo = {
     username: $("#logUsername").val(),
     password: $("#logPassword").val()
   }
   var logInfoString = JSON.stringify(logInfo);
+  var req = new XMLHttpRequest();
 
-  req.open('GET', '/login', true);
+  req.open('GET', '/login', false);
   req.setRequestHeader("Content-Type", "application/json");
+  // get info from server (if user exists/password correct)
   req.onreadystatechange = function () {
       if (req.readyState === 4 && req.status === 200) {
         logSuccess = JSON.parse(req.responseText);
@@ -30,38 +24,68 @@ function userLog() {
   };
   req.send(logInfoString);
 
-
-  // get info from server (if user exists/password correct)
-  // set user login to true
-  // set userid
-  // got to home page if logged in
-    //window.location.href = 'frontpage.html'
-
+  // dealing with info from server
+  if (logSuccess.userexists == "false") {
+      sessionStorage.setItem("loginMessage","invalid username or password");
+  }
+  else if (logSuccess.userexists == "true") {
+    sessionStorage.setItem("userLogin", "true");
+    sessionStorage.setItem("user", $("#logUsername").val());
+    window.location.href = 'managepage.html';
+  }
+  else {
+    sessionStorage.setItem("loginMessage","login failed");
+  }
 }
 
 function userReg() {
-  let username = $("#regUsername").val();
-  let password = $("#regPassword").val();
-
-  if (username == '' || password == ''){
-    sessionStorage.setItem("loginMessage","username or password cannot be empty");
+  var logSuccess;
+  /* logSuccess form
+  logSuccess = {
+    userexists: <true or false value>
   }
-  else if (username.contains(' ') || password.contains(' ')){
-    sessionStorage.setItem("loginMessage","username and password cannot contain a space");
+  */
 
+  var logInfo = {
+    username: $("#logUsername").val(),
+    password: $("#logPassword").val()
+  }
+  var logInfoString = JSON.stringify(logInfo);
+
+  if (logInfo.username == '' || logInfo.password == ''){
+    sessionStorage.setItem("loginMessage","username or password cannot be empty");
+    return;
+  }
+  else if (logInfo.username.contains(' ') || logInfo.password.contains(' ')){
+    sessionStorage.setItem("loginMessage","username and password cannot contain a space");
+    return;
   }
 
   // sending information to server
-  /*
+  var req = new XMLHttpRequest();
+  req.open('GET', '/login', false);
+  req.setRequestHeader("Content-Type", "application/json");
+  // get info from server (if user exists/password correct)
+  req.onreadystatechange = function () {
+      if (req.readyState === 4 && req.status === 200) {
+        logSuccess = JSON.parse(req.responseText);
+      }
+  };
+  req.send(logInfoString);
 
+  // dealing with info from server
+  if (logSuccess.userexists == "false") {
+      sessionStorage.setItem("loginMessage","invalid username or password");
+  }
+  else if (logSuccess.userexists == "true") {
+    sessionStorage.setItem("userLogin", "true");
+    sessionStorage.setItem("user", $("#logUsername").val());
+    window.location.href = 'managepage.html';
+  }
+  else {
+    sessionStorage.setItem("loginMessage","login failed");
+  }
 
-  */
-
-  // get info from server (if user already exists)
-  // set user login to true
-  // set userid
-  // got to home page if registered
-    //window.location.href = 'frontpage.html'
 }
 
 $(document).ready(function(){
