cdd5232d34d36a6a6b63d6245ae85980bc2a9127
sunahkim15 <sunah.kim@ucalgary.ca>
2018-12-04 13:00:25 -0700

    able to show recipe on the webpage

--- Frontend/js/viewrecipepage.js
+++ Frontend/js/viewrecipepage.js
@@ -8,46 +8,167 @@ var recipeData;
 recipeData = {
   title: <title>,
   number: <num>
-  description: <d>,
+  author: <name>
+  description: <des> (not now)
   foodType: [ft, ft, ....],
   ingredients: [i,i,....i],
+  ingAmount: [ia, ia...],
   cookware: [c,c...c],
   steps: <s>,
   favourite: <true/false>
 }
 */
 
+var recipeReviewData;
+/* recipeReviewData form
+
+recipeReviewData = {
+  author: [],
+  text: [],
+  point:[]
+}
+*/
+
+var userReviewData;
+/* reviewData form
+
+uwerReviewData = {
+  author: <username>,
+  text: <review text>,
+  point: <out of 5>
+}
+*/
+var addOrEdit; // ADD if new review, EDIT if review exists
+
 function addFavourite() {
 
 }
 
-function addReview() {
-
+function addOrEdit() {
+  if (addOrEdit == "ADD") {
+    addReview();
+  }
+  else if (addOrEdit == "EDIT") {
+    editReview();
+  }
+  else {
+    console.log("error in determining add or edit review");
+  }
 }
 
-function getData() {
+function addReview() {
   var recipeSelectString = JSON.stringify(recipeSelected);
 
   // get recipe data from server
   var req = new XMLHttpRequest();
   req.open('GET', '/recipeData', true);
   req.setRequestHeader("Content-Type", "application/json");
-  // get info from server (if user exists/password correct)
   req.onreadystatechange = function () {
       if (req.readyState === 4 && req.status === 200) {
-        recipeList = JSON.parse(req.responseText); //RETURNDATA
+        recipeData = JSON.parse(req.responseText); //RETURNDATA
       }
   };
-  req.send(userSearchString);
+  req.send(recipeSelectString);
+}
+
+function editReview() {
+  var recipeSelectString = JSON.stringify(recipeSelected);
+
+  // get recipe data from server
+  var req = new XMLHttpRequest();
+  req.open('GET', '/recipeData', true);
+  req.setRequestHeader("Content-Type", "application/json");
+  req.onreadystatechange = function () {
+      if (req.readyState === 4 && req.status === 200) {
+        recipeData = JSON.parse(req.responseText); //RETURNDATA
+      }
+  };
+  req.send(recipeSelectString);
+}
+
+function deleteReview() {
+
+}
+
+function getRecipeData() {
+  var recipeSelectString = JSON.stringify(recipeSelected);
+
+  // get recipe data from server
+  var req = new XMLHttpRequest();
+  req.open('GET', '/recipeData', true);
+  req.setRequestHeader("Content-Type", "application/json");
+  req.onreadystatechange = function () {
+      if (req.readyState === 4 && req.status === 200) {
+        recipeData = JSON.parse(req.responseText); //RETURNDATA
+      }
+  };
+  req.send(recipeSelectString);
+}
+
+function showRecipe() {
+  recipeData = {
+    title: "My Recipe",
+    author: "John", //username
+    number: 1,
+    foodType: ["noodle", "fushion", "dinner"],
+    ingredients: ["bacon", "carrot", "onion"],
+    ingAmount: ["4", "1 cup", "1"],
+    ingType: ["meat", "vegi", "vegi"],
+    cookware: ["pot", "spoon"],
+    steps: "Cook the bacon in the pot and add carrots and onions stir with spoooon",
+    favourite: "true"
+  }
+
+  $("#recipeTitle").text(recipeData.title);
+  $("#recipeAuthor").text("Made by: " + recipeData.author);
+  $("#steps").text(recipeData.steps);
+  for (i=0;i<recipeData.ingredients.length;i++) {
+    $("#ingredients").append(recipeData.ingredients[i] + " " + recipeData.ingAmount[i] + "<br>");
+  }
+  for (i=0;i<recipeData.cookware.length;i++) {
+    $("#cookware").append(recipeData.cookware[i] + "<br>");
+  }
+}
+
+function getReviews() {
+  var recipeSelectString = JSON.stringify(recipeSelected);
+
+  // get recipe data from server
+  var req = new XMLHttpRequest();
+  req.open('GET', '/getReviews', true);
+  req.setRequestHeader("Content-Type", "application/json");
+  req.onreadystatechange = function () {
+      if (req.readyState === 4 && req.status === 200) {
+        recipeReviewData = JSON.parse(req.responseText); //RETURNDATA
+      }
+  };
+  req.send(recipeSelectString);
+}
+
+function showReviews() {
+// determine addOrEdit review
+// if EDIT review, show user review in the user reivew input box, show delete review button
 }
 
 $(document).ready(function(){
-  getData();
+  $("#deleteReview").hide();
+  getRecipeData();
+  showRecipe();
+  getReviews();
+  showReviews();
+
 
   if (sessionStorage.getItem("userLogin") == "true") {
-    // show favourite button
+    $("#favButton").show();
+    $(".addReviewContainer").show();
     // allow add favourite when clicked
     // show add comment button
     // allow add comment
+    $("#addReview").click(addOrEdit);
+
+  }
+  else {
+    $("#favButton").hide();
+    $(".addReviewContainer").hide();
   }
 });
